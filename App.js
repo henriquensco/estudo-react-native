a57be6f71e06bbbeeda93e595eeec26bfe492320
henriquensco <nascimentocostahenrique@gmail.com>
2022-04-15 17:48:07 -0300

    Props e States

--- App.js
+++ App.js
@@ -1,36 +1,38 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { SafeAreaView, StatusBar, Text, StyleSheet } from 'react-native';
 import styled from 'styled-components/native';
 
+/* 
+Props e States
+*/
+
 const Page = styled.SafeAreaView`
   flex:1;
-  backgroundColor: #0F110C;
+  backgroundColor: #CED4DA;
+  justify-content: center;
+  align-items: center;
 `;
 
-const Header = styled.View`
-  display:flex;
-  flex-direction: row;
-  justify-content: space-between;
-  backgroundColor: #eee;
-`;
 
-const Quadrado = styled.View`
-  height: 50px;
-  width: 50px;
-  backgroundColor: ${props=>props.cor};
-`;
+const Hello = (props) => {
+
+  const [name, setName] = useState("Henrique");
+
+  return(
+    <Text>Nome: {name}</Text>
+    /*<Text>{props.frase}</Text>*/
+    /* <Text>Frase Qualquer</Text> */
+  );
+};
+
 
 export default () => {
   return (
     <Page>
       <StatusBar hidden />
-      <Header>
-        <Quadrado cor="red"></Quadrado>
-        <Quadrado cor="green"></Quadrado>
-        <Quadrado cor="blue"></Quadrado>
 
-        <Quadrado cor="black"></Quadrado>
-      </Header>
+      <Hello frase="Seja bem vindo!"/>
+
     </Page>
   );
 };
