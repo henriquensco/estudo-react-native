d3aafdc12ea2ed82726101482380281d077cd1a1
henriquensco <nascimentocostahenrique@gmail.com>
2022-04-16 12:06:55 -0300

    Calculadora de gorjeta

--- App.js
+++ App.js
@@ -8,99 +8,107 @@ Props e States
 
 const Page = styled.SafeAreaView`
   flex:1;
-  backgroundColor: #002E2C;
-  justify-content: center;
+  backgroundColor: #fff;
   align-items: center;
+`;
+
+const HeaderText = styled.Text`
+  fontSize: 25px;
 `;
 
 const Input = styled.TextInput`
-  height: 40px;
-  width: 200px;
-  border: 2px solid #f1f1f1;
+  width: 90%;
+  height: 50px;
+  font-size: 18px;
+  background-color: #eee;
+  margin-top: 20px;
+  border-radius: 4px;
   padding: 10px;
-  border-radius: 5px;
-  background-color: #fff;
 `;
 
-const Texto = styled.Text`
-  fontSize: 20px;
+const CalcButton = styled.Button`
+  margin-top: 20px;
+`;
+
+const ResultArea = styled.View`
+  margin-top: 20px;
+  background-color: #eee;
+  padding: 20px;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const ResultItemTitle = styled.Text`
+  font-size: 18px;
   font-weight: bold;
-  color: #fff;
-  text-align: center;
   padding: 5px;
 `;
 
-const Quadrado = styled.View`
-  backgroundColor: #fff;
-  width: 200px;
-  height: 200px;
-  padding: 5px 5px;
-  justify-content: center;
-  align-items: center;
-  border: 1px solid #000;
-  border-radius: 4px;
+const ResultItem = styled.Text`
+  font-size: 15px;
 `;
 
-const Hello = (props) => {
+const PctArea = styled.View`
+  flex-direction: row;
+  margin: 20px;
+  padding: 5px 5px;
+`;
 
-  const [ name, setName ] = useState("novo");
-  const [ backupName, setBackupName] = useState("");
-  const [ mostrar, setMostrar ] = useState(false);
-
-  const handleClick = () => {
-    setBackupName(name);
-    
-    //setMostrar(true);
-    if(mostrar == true) {
-      setMostrar(false);
-    } else {
-      setMostrar(true);
-    }
-
-  };
-
-  return(
-    <View>
-      <Texto>Qual o seu nome?</Texto>
-
-      <Input onChangeText={(e) => {setName(e)}} />
-
-      <Button title={mostrar ? 'Esconder Nome' : 'Mostrar Nome'} onPress={handleClick}/>
-
-      {/* <Button title="Salvar" onPress={handleClick}/> */}
-
-      {/* <Text style={{
-        fontSize: 20,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        color: '#fff'
-      }}>{name}</Text> */}
-
-      {mostrar &&
-        <Quadrado>
-          <Text style={{
-            fontSize: 20,
-            fontWeight: 'bold',
-            textAlign: 'center',
-            color: '#000'
-          }}>Seu nome é: {backupName}</Text>
-        </Quadrado>
-      }
-
-    </View>
-    /* <Text>Nome: {name}</Text> */
-    /* <Text>{props.frase}</Text> */
-    /* <Text>Frase Qualquer</Text> */
-  );
-};
-
+const PctItem = styled.Button`
+`;
 
 export default () => {
+
+  const [ bill, setBill ] = useState("");
+  const [ tip, setTip ] = useState(0);
+  const [ pct, setPct ] = useState(10);
+
+  const calc = () => {
+    let nBill = parseFloat(bill);
+    
+    if(nBill) {
+      setTip((pct/100) * nBill);
+    } else {
+      alert("Digite o valor da conta!");
+    }
+  };
+
   return (
     <Page>
-      {/* <StatusBar hidden /> */}
+      <StatusBar hidden />
 
-      <Hello frase="Seja bem vindo!"/>
+      <HeaderText>Calculadora de Gorjeta</HeaderText>
+      <Input 
+        placeholder="Quanto deu a conta?"
+        placeholderTextColor="#000"
+        keyboardType="numeric"
+        value={bill}
+        onChangeText={e=>setBill(e)}
+      />
+
+      <PctArea>
+        <PctItem title="5%" onPress={() => setPct(5)}/>
+        <PctItem title="10%" onPress={() => setPct(10)}/>
+        <PctItem title="15%" onPress={() => setPct(15)}/>
+        <PctItem title="20%" onPress={() => setPct(20)}/>
+      </PctArea>
+
+      <CalcButton title={`Calcular ${pct}%`} onPress={calc} />
+
+      {tip > 0 &&
+      <ResultArea>
+        <ResultItemTitle>Valor da Conta</ResultItemTitle>
+        <ResultItem>R$ {parseFloat(bill).toFixed(2)}</ResultItem>
+
+        <ResultItemTitle>Valor da Gorjeta</ResultItemTitle>
+        <ResultItem>R$ {tip.toFixed(2)}</ResultItem>
+
+        <ResultItemTitle>Valor da Total</ResultItemTitle>
+        <ResultItem>R$ {(parseFloat(bill) + tip).toFixed(2)}</ResultItem>
+
+      </ResultArea>
+      }
 
     </Page>
   );
