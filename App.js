8b86edc82123c5c0ec53ccd077b4ac2507fb3d4b
henriquensco <nascimentocostahenrique@gmail.com>
2022-04-16 09:58:08 -0300

    Components, props e states

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { SafeAreaView, StatusBar, Text, StyleSheet } from 'react-native';
+import { SafeAreaView, View, StatusBar, Text, StyleSheet, Alert } from 'react-native';
 import styled from 'styled-components/native';
 
 /* 
@@ -8,19 +8,45 @@ Props e States
 
 const Page = styled.SafeAreaView`
   flex:1;
-  backgroundColor: #CED4DA;
+  backgroundColor: #002E2C;
   justify-content: center;
   align-items: center;
 `;
 
+const Input = styled.TextInput`
+  height: 40px;
+  width: 200px;
+  border: 2px solid #f1f1f1;
+  padding: 10px;
+  border-radius: 5px;
+  background-color: #fff;
+`;
+
+const Texto = styled.Text`
+  fontSize: 20px;
+  font-weight: bold;
+  color: #fff;
+  text-align: center;
+  padding: 5px;
+`;
 
 const Hello = (props) => {
 
-  const [name, setName] = useState("Henrique");
+  const [name, setName] = useState("novo");
 
   return(
-    <Text>Nome: {name}</Text>
-    /*<Text>{props.frase}</Text>*/
+    <View>
+      <Texto>Escreva algo:</Texto>
+      <Input onChangeText={(e) => {setName(e)}} />
+      <Text style={{
+        fontSize: 20,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: '#fff'
+      }}>{name}.</Text>
+    </View>
+    /* <Text>Nome: {name}</Text> */
+    /* <Text>{props.frase}</Text> */
     /* <Text>Frase Qualquer</Text> */
   );
 };
@@ -29,7 +55,7 @@ const Hello = (props) => {
 export default () => {
   return (
     <Page>
-      <StatusBar hidden />
+      {/* <StatusBar hidden /> */}
 
       <Hello frase="Seja bem vindo!"/>
 
