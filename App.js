062b5a73ebe272433fae628a8895399c981c1f32
henriquensco <nascimentocostahenrique@gmail.com>
2022-04-17 17:12:56 -0300

    Funcionalidades do teclado de acordo com a tela

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import React, { useState, useEffect } from 'react';
-import { SafeAreaView, View, StatusBar, Text, StyleSheet, Button } from 'react-native';
+import React, { useEffect } from 'react';
+import { StatusBar } from 'react-native';
+import { Platform } from 'react-native-web';
 import styled from 'styled-components/native';
 
 /* 
@@ -10,107 +11,45 @@ const Page = styled.SafeAreaView`
   flex:1;
   backgroundColor: #fff;
   align-items: center;
-`;
-
-const HeaderText = styled.Text`
-  fontSize: 25px;
+  justify-content: center;
 `;
 
 const Input = styled.TextInput`
   width: 90%;
-  height: 50px;
-  font-size: 18px;
-  background-color: #eee;
-  margin-top: 20px;
+  height: 45px;
+  border: 1px solid #000;
   border-radius: 4px;
   padding: 10px;
 `;
 
-const CalcButton = styled.Button`
-  margin-top: 20px;
+const Logo = styled.View`
+  width: 100px;
+  height: 100px;
+  background-color: #000;
+  margin: 15px;
 `;
 
-const ResultArea = styled.View`
-  margin-top: 20px;
-  background-color: #eee;
-  padding: 20px;
-  justify-content: center;
-  align-items: center;
+const KeyboardArea = styled.KeyboardAvoidingView`
+  flex: 1;
   width: 100%;
+  align-items: center;
+  justify-content: center;
 `;
-
-const ResultItemTitle = styled.Text`
-  font-size: 18px;
-  font-weight: bold;
-  padding: 5px;
-`;
-
-const ResultItem = styled.Text`
-  font-size: 15px;
-`;
-
-const PctArea = styled.View`
-  flex-direction: row;
-  margin: 20px;
-  padding: 5px 5px;
-`;
-
-const PctItem = styled.Button``;
 
 export default () => {
 
-  const [ bill, setBill ] = useState("");
-  const [ tip, setTip ] = useState(0);
-  const [ pct, setPct ] = useState(10);
-
-  const calc = () => {
-    let nBill = parseFloat(bill);
-    
-    if (nBill) {
-      setTip((pct/100) * nBill);
-    } else {
-      alert("Digite o valor da conta!");
-    }
-  };
-
-  useEffect(() => {
-    calc();
-  }, [pct]);
+  /* useEffect(() => {
+    alert('SO: ' + Platform.OS);
+  }, []); */
 
   return (
     <Page>
       <StatusBar hidden />
 
-      <HeaderText>Calculadora de Gorjeta</HeaderText>
-      <Input 
-        placeholder="Quanto deu a conta?"
-        placeholderTextColor="#000"
-        keyboardType="numeric"
-        value={bill}
-        onChangeText={e=>setBill(e)}
-      />
-
-      <PctArea>
-        <PctItem title="5%" onPress={() => setPct(5)}/>
-        <PctItem title="10%" onPress={() => setPct(10)}/>
-        <PctItem title="15%" onPress={() => setPct(15)}/>
-        <PctItem title="20%" onPress={() => setPct(20)}/>
-      </PctArea>
-
-      <CalcButton title={`Calcular ${pct}%`} onPress={calc} />
-
-      {tip > 0 &&
-      <ResultArea>
-        <ResultItemTitle>Valor da Conta</ResultItemTitle>
-        <ResultItem>R$ {parseFloat(bill).toFixed(2)}</ResultItem>
-
-        <ResultItemTitle>Valor da Gorjeta</ResultItemTitle>
-        <ResultItem>R$ {tip.toFixed(2)}</ResultItem>
-
-        <ResultItemTitle>Valor da Total</ResultItemTitle>
-        <ResultItem>R$ {(parseFloat(bill) + tip).toFixed(2)}</ResultItem>
-      </ResultArea>
-      }
+      <KeyboardArea behavior={Platform.OS == 'ios' ? 'padding' : null}>
+        <Logo />
+        <Input />
+      </KeyboardArea>
 
     </Page>
   );
