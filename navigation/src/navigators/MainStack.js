6a60af8f59c9f5aea1de38dba849861c630e586b
henriquensco <nascimentocostahenrique@gmail.com>
2022-04-24 10:54:47 -0300

    Personalizacao de header

--- navigation/src/navigators/MainStack.js
+++ navigation/src/navigators/MainStack.js
@@ -3,13 +3,39 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import HomeScreen from '../pages/HomeScreen';
 import AboutScreen from '../pages/AboutScreen';
+import { backgroundColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
 
 const MainStack = createStackNavigator();
 
 export default () => {
     return (
-        <MainStack.Navigator>
-            <MainStack.Screen name='Home' component={HomeScreen} />
+        <MainStack.Navigator screenOptions={{
+            headerTitleAlign: 'center',
+            headerStyle: {
+                backgroundColor: '#023047',
+                height: 100
+            },
+            headerTitleStyle: {
+                color: '#fff',
+                fontSize: 16,
+                fontWeight: 'bold'
+            },
+            headerBackTitle: 'Voltar',
+            headerTruncatedBackTitle: 'Voltar'
+        }}>
+            <MainStack.Screen name='Home' component={HomeScreen} options={{
+                title: 'Início',
+                /* headerTitleAlign: 'center',
+                headerStyle: {
+                    backgroundColor: '#023047',
+                    height: 200
+                },
+                headerTitleStyle: {
+                    color: '#fff',
+                    fontSize: 23,
+                    fontWeight: 'bold'
+                } */
+            }} />
             <MainStack.Screen name='About' component={AboutScreen} />
         </MainStack.Navigator>
     );
